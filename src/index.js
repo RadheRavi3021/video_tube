7991ea85b4864fb406934204313b7ae008b53c1a
Radhe Ravi <90458033+RadheRavi3021@users.noreply.github.com>
2023-12-31 01:04:52 +0530

    connection of database

---
+++ src/index.js
@@ -0,0 +1,38 @@
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// const app = express();
+/*
+function connectDB() {
+
+}
+connectDB()
+*/
+/*
+
+;( async ()=>{
+    try{
+        await mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.error("Error: ",error)
+        throw error
+    }
+})();
+*/
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
